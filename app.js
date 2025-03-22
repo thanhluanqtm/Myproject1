1c02f058b2f6d64a57c2d7be0cb405a6cebb339c
thanhluanqtm <luanm2524011@gstudent.ctu.edu.vn>
2025-03-22 10:15:04 +0700

    upweb

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html>
-<html lang="vi">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thông tin h?c viên</title>
+  res.end('
     <style>
         table {
             width: 100%;
